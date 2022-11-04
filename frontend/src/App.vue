f092d43f7f055a4a971e092662d5609990ed4ef4
alvarogfn <alvarogfn@hotmail.com>
2022-11-04 19:53:36 -0300

    first commit!

---
+++ frontend/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur sint
+      libero commodi? Quo, ipsam facilis. Molestias eligendi porro tenetur, rem
+      asperiores non omnis provident? Debitis ipsa blanditiis quidem commodi
+      reprehenderit?
+    </div>
+    <div>
+      <pie-chart
+        :chart-data="{
+          labels: ['VueJs', 'EmberJs', 'ReactJs', 'AngularJs'],
+          datasets: [
+            {
+              backgroundColor: ['#41B883', '#E46651', '#00D8FF', '#DD1B16'],
+              data: [40, 20, 80, 10],
+            },
+          ],
+        }"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+  import { Pie as PieChart } from "vue-chartjs";
+
+  import {
+    Chart as ChartJS,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale,
+    ArcElement,
+  } from "chart.js";
+
+  ChartJS.register(
+    ArcElement,
+    Title,
+    Tooltip,
+    Legend,
+    BarElement,
+    CategoryScale,
+    LinearScale
+  );
+</script>
+
+<style scoped></style>
